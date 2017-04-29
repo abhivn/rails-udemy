22e0434c2da0fca647aa0bcf0d27ab3bb9947170
Rails Udemy <abhigeek8@gmail.com>
2017-04-29 23:55:17 +0530

    extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -10,7 +12,7 @@ class ArticlesController < ApplicationController
 
   def create
     #render plain: params[:article].inspect
-    @article = Article.new(article_params)
+    # @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created..."
       redirect_to article_path(@article)
@@ -23,16 +25,16 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
 
   end
 
   def update
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated!"
       redirect_to article_path(@article)
@@ -43,7 +45,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted!"
     redirect_to articles_path
@@ -51,6 +53,12 @@ class ArticlesController < ApplicationController
 
 
   private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+
     def article_params
       params.require(:article).permit(:title, :description)
 
