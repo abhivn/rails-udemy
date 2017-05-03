8049304d24a599a60ea8f34c178c538c31417d07
Rails Udemy <abhigeek8@gmail.com>
2017-05-04 02:03:57 +0530

    add association btw user and article and add debugger

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
+    has_many :articles
+    before_save {self.email = email.downcase}
+
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 25, minimum: 4}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502192042) do
+ActiveRecord::Schema.define(version: 20170503192259) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
