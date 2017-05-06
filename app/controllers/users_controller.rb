5aa5b1c2aa93d0185a6d68e49c6058cddc3ec99c
Rails Udemy <abhigeek8@gmail.com>
2017-05-07 01:52:19 +0530

    add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
 
@@ -32,7 +36,7 @@ end
 
 def show
   @user = User.find(params[:id])
-  
+
 end
 
   private
